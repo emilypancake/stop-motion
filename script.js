c04c3b94f73e6875a7836f1b2c8eb0461f6ab171
Emily Pan <pancakerula@gmail.com>
2026-05-15 23:24:17 -0700

    js initial

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+
 const { FFmpeg } = window.FFmpegWeb;
 const ffmpeg = new FFmpeg();
+
+await ffmpeg.load();
+
+ffmpeg.on("progress", ({ progress, time}) => {
+    // update html with progress, time
+});
+ffmpeg.on("log", ({type, messsage}) => {
+    // update html with possible error logs
+});
+
+const result = await ffmpeg.exec(['-version']);
+//update html with result
